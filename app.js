9e01216a38a39ec4447d92b5d69da30999040ddd
Luke Laurie <lukelaurie@arizona.edu>
2023-02-23 18:43:23 -0700

    worked on secondary branch

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(express.static("public"));
 
+// this is the secondary branch
+
 /*
 Sends the main html page when get request recieved
 */
